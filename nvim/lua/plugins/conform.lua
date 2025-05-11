4a4e2211894ef85812ce2743fd4a44bc57ab628d
peoxin <peoxin@outlook.com>
2025-05-11 19:45:08 +0800

    update waybar config

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -9,19 +9,24 @@ return {
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
+			sh = { "shfmt" },
 			python = { "ruff_organize_imports", "ruff_format" },
 			rust = { "rustfmt", lsp_format = "fallback" },
 			javascript = { "prettier" },
-			sh = { "shfmt" },
+			css = { "prettier" },
+			json = { "prettier" },
+			jsonc = { "prettier" },
 			yaml = { "prettier" },
 			toml = { "taplo" },
 			markdown = { "prettier" },
 			-- "_" means filetypes that don't have other formatters configured
 			["_"] = { "trim_newlines", "trim_whitespace" },
 		},
+		default_format_opts = {
+			lsp_format = "never",
+		},
 		format_on_save = {
 			timeout_ms = 500,
-			lsp_format = "fallback",
 		},
 	},
 }
