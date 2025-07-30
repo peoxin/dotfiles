72aecf01a8ec544ad349144dd5133ef752134bee
peoxin <peoxin@outlook.com>
2025-07-30 18:19:44 +0800

    refactor(linux): rewrite user scripts

--- linux/scripts/network-connect.sh
+++ linux/scripts/network-connect.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/sh
+#!/usr/bin/env bash
 
-connection=$(nmcli connection show | awk '{print $1}' | rg -v "NAME|lo" | fuzzel -d -p "Select connection: ")
+connection=$(nmcli connection show | awk '{print $1}' | rg -v "NAME|lo" | fuzzel -d -p "Connect: ")
 [ -z "$connection" ] && exit 0
 
 nmcli connection up $connection
