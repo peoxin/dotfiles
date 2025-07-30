f3f444c1988d048bfe471889a7b56a3b907dc510
peoxin <peoxin@outlook.com>
2025-07-30 18:19:44 +0800

    chore(nvim): add formatters in conform plugin

--- common/nvim/lua/plugins/conform.lua
+++ common/nvim/lua/plugins/conform.lua
@@ -10,15 +10,19 @@ return {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			sh = { "shfmt" },
+			just = { "just" },
 			python = { "ruff_organize_imports", "ruff_format" },
-			rust = { "rustfmt", lsp_format = "fallback" },
+			rust = { "rustfmt" },
 			javascript = { "prettier" },
+			typescript = { "prettier" },
 			css = { "prettier" },
+			html = { "prettier" },
 			json = { "prettier" },
 			jsonc = { "prettier" },
 			yaml = { "prettier" },
 			toml = { "taplo" },
 			markdown = { "prettier" },
+			qml = { "qmlformat" },
 			-- "_" means filetypes that don't have other formatters configured
 			["_"] = { "trim_newlines", "trim_whitespace" },
 		},
@@ -27,6 +31,13 @@ return {
 		},
 		format_on_save = {
 			timeout_ms = 500,
+		},
+		formatters = {
+			qmlformat = {
+				command = "qmlformat",
+				args = { "-i", "$FILENAME" },
+				stdin = false,
+			},
 		},
 	},
 }
