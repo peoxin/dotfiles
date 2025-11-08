6bc6411d1a798d2fa48d0ff950fefbd234f79c4f
peoxin <peoxin@outlook.com>
2025-11-09 06:22:34 +0800

    fix: macos

--- home/common/yazi/default.nix
+++ home/common/yazi/default.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   programs.yazi = {
     enable = true;
     shellWrapperName = "y";
@@ -11,12 +10,12 @@
     keymap = {
       mgr.prepend_keymap = [
         {
-          on = [ "C" ];
+          on = ["C"];
           run = "plugin ouch";
           desc = "Compress with ouch";
         }
         {
-          on = [ "g" "p" ];
+          on = ["g" "p"];
           run = "cd ~/Projects";
           desc = "Go ~/Projects";
         }
@@ -36,18 +35,57 @@
         }
       ];
       plugin.prepend_previewers = [
-        { mime = "application/*zip";            run = "ouch"; }
-        { mime = "application/x-tar";           run = "ouch"; }
-        { mime = "application/x-bzip2";         run = "ouch"; }
-        { mime = "application/x-7z-compressed"; run = "ouch"; }
-        { mime = "application/x-rar";           run = "ouch"; }
-        { mime = "application/vnd.rar";         run = "ouch"; }
-        { mime = "application/x-xz";            run = "ouch"; }
-        { mime = "application/xz";              run = "ouch"; }
-        { mime = "application/x-zstd";          run = "ouch"; }
-        { mime = "application/zstd";            run = "ouch"; }
-        { mime = "application/java-archive";    run = "ouch"; }
+        {
+          mime = "application/*zip";
+          run = "ouch";
+        }
+        {
+          mime = "application/x-tar";
+          run = "ouch";
+        }
+        {
+          mime = "application/x-bzip2";
+          run = "ouch";
+        }
+        {
+          mime = "application/x-7z-compressed";
+          run = "ouch";
+        }
+        {
+          mime = "application/x-rar";
+          run = "ouch";
+        }
+        {
+          mime = "application/vnd.rar";
+          run = "ouch";
+        }
+        {
+          mime = "application/x-xz";
+          run = "ouch";
+        }
+        {
+          mime = "application/xz";
+          run = "ouch";
+        }
+        {
+          mime = "application/x-zstd";
+          run = "ouch";
+        }
+        {
+          mime = "application/zstd";
+          run = "ouch";
+        }
+        {
+          mime = "application/java-archive";
+          run = "ouch";
+        }
       ];
     };
+  };
+
+  catppuccin.yazi = {
+    enable = true;
+    flavor = "mocha";
+    accent = "blue";
   };
 }
